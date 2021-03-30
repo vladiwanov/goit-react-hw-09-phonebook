5042f8698b29ad2fb4f12a36a14ce3cb9b25610a
Volodymyr Ivanov <3298901@gmail.com>
2021-03-31 00:40:52 +0300

    Updates

---
+++ static/js/5.8b8d9b2c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[5],{106:function(e,t,n){"use strict";n.r(t);var o=n(96),c=n.n(o),h=n(1);t.default=function(){return Object(h.jsx)(h.Fragment,{children:Object(h.jsx)("h1",{className:c.a.h1,children:" \u0413\u043b\u0430\u0432\u043d\u0430\u044f \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u0430 "})})}},96:function(e,t,n){e.exports={h1:"HomePageView_h1__2Am3q"}}}]);
+//# sourceMappingURL=5.8b8d9b2c.chunk.js.map
